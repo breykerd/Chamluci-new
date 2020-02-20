1c398712b8fa9983e6b6a8d38500eacb869ba67d
breykerd <breykerd@gmail.com>
2020-02-20 11:27:55 -0500

    agregado iconos y personalizado parte de la cabesera

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
 	<meta charset="UTF-8">
 	<title>Corporación Chamluci - Líderes en línea institucional</title>
 	<link rel="stylesheet/less" type="text/css" href="assets/css/style.less" />
+	<link rel="stylesheet/less" type="text/css" href="assets/fonts/fontawesome/css/font-awesome.min.css" />
 	<script src="assets/js/less.min.js" ></script>
 </head>
 <body>
+<!-- start custom menu-->	
+<header class="site-header">
+	<div class="top-bar">
+		<div class="container">
+			<div class="row">
+				<div class="wt-topbar-center">
+					<ul>
+						<li> <i class="fa fa-envelope-o"></i></li>
+						<li>
+							 <a href="mailto:Ventas@corporacionchamluci.com">Ventas@corporacionchamluci.com</a>
+						</li>
+					</ul>
+
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
 <div class="menu">
 	<ul>
 		<li>Inicio</li>
@@ -16,6 +37,11 @@
 		<li>Contactos</li>
 	</ul>
 </div>
+
+
+</header>
+<!-- end custom menu --> 
+
 
 </body>
 </html>
