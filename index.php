a67fdb2fef4e39fe9032b320121579ed7d1478e5
breykerd <breykerd@gmail.com>
2020-02-20 13:49:00 -0500

    agregado estilos para boton primario call to action

--- index.php
+++ index.php
@@ -4,44 +4,64 @@
 	<meta charset="UTF-8">
 	<title>Corporación Chamluci - Líderes en línea institucional</title>
 	<link rel="stylesheet/less" type="text/css" href="assets/css/style.less" />
+	<link rel="stylesheet/less" type="text/css" href="assets/css/responsive.less" />
 	<link rel="stylesheet/less" type="text/css" href="assets/fonts/fontawesome/css/font-awesome.min.css" />
 	<script src="assets/js/less.min.js" ></script>
 </head>
 <body>
 <!-- start custom menu-->	
 <header class="site-header">
+
 	<div class="top-bar">
 		<div class="container">
 			<div class="row">
 				<div class="wt-topbar-center">
 					<ul>
-						<li> <i class="fa fa-envelope-o"></i></li>
 						<li>
 							 <a href="mailto:Ventas@corporacionchamluci.com">Ventas@corporacionchamluci.com</a>
 						</li>
 					</ul>
-
 				</div>
-
 			</div>
-
 		</div>
 	</div>
+	<!-- -------------------- -->
+<div class="header">
 
-<div class="menu">
-	<ul>
-		<li>Inicio</li>
-		<li>La Empresa</li>
-		<li class="productos">Productos</li>
-		<li>Blogs</li>
-		<li>Contactos</li>
-	</ul>
+
+<div class="container">
+	<div class="row">
+
+	<div class="Superior">
+		<div class="logo-Header"> <img src="assets/images/logo.png" alt=""></div>
+		<div class="busqueda"><input type="text"> <i class="fa fa-search"></i> </div>
+		<div></div>
+		<div class="central"></div>
+		<div class="redes sociales"></div>
+
+	</div>
+
+	<div class="inferior menu"> 
+		<ul>
+			<li class="secciones"><a href="">Inicio</a></li>
+			<li class="secciones"><a href="">La Empresa</a></li>
+			<li class="productos"><a href="">Productos</a></li>
+			<li class="secciones"><a href="">Blogs</a></li>
+			<li class="secciones"><a href="">Contactos</a></li>
+		</ul>
+	</div>
+	
+	</div>
 </div>
+ 
+
 
 
 </header>
 <!-- end custom menu --> 
-
+<div>
+	<button class="buttom">Cotización</button>
+</div>
 
 </body>
 </html>
