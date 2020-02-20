ff67047b186e0fbf3dd1bdc111d9be5491d4519a
breykerd <breykerd@gmail.com>
2020-02-20 09:55:29 -0500

    inicio de proyecto (pagina web Corporación Chamluci)

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Corporación Chamluci - Líderes en línea institucional</title>
+	<link rel="stylesheet/less" type="text/css" href="assets/css/style.less" />
+	<script src="assets/js/less.min.js" ></script>
+</head>
+<body>
+<div class="menu">
+	<ul>
+		<li>Inicio</li>
+		<li>La Empresa</li>
+		<li class="productos">Productos</li>
+		<li>Blogs</li>
+		<li>Contactos</li>
+	</ul>
+</div>
+
+</body>
+</html>
