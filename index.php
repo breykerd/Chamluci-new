554c32249993a0d3f2f21910886e7f7dce975d1c
breykerd <breykerd@gmail.com>
2020-02-24 15:47:20 -0500

    terminado menu superior en resolucion 1336px y 768px

--- index.php
+++ index.php
@@ -38,32 +38,37 @@
 <div class="division">
 &nbsp;
 </div>
-		<div class="central"><i class="fa fa-phone"></i><input type="text" value="01-222 2312" disabled> </div>
+		<div class="central"><i class="fa fa-phone"></i>
+		<a href="tel:012222312" >Central: (01)-222 2312 </a>
+</div>
 		<div class="redes-sociales">
-			<i class="fa fa-facebook-square"></i>
-			<i class="fa fa-twitter"></i>
-			<i class="fa fa-youtube"></i>
-			<i class="fa fa-instagram"></i>
+			<i class="fa fa-facebook-square iconSocial"></i>
+			<i class="fa fa-twitter iconSocial"></i>
+			<i class="fa fa-youtube iconSocial"></i>
+			<i class="fa fa-instagram iconSocial"></i>
 		</div>
 
 	</div>
-
+</div>
+<div class="row">
 	<div class="inferior menu"> 
 		<ul>
 			<li class="secciones"><a href="">Inicio</a></li>
 			<li class="secciones"><a href="">La Empresa</a></li>
-			<li class="productos"><a href="">Productos</a></li>
+			<li class="productos"><a href="">Productos</a> </li>
 			<li class="secciones"><a href="">Blogs</a></li>
 			<li class="secciones"><a href="">Contactos</a></li>
 		</ul>
 	</div>
+	<div class="carrito">
+<i class="fa fa-shopping-cart"></i>
+<div class="cantidad">
+5
+</div>
+	</div>
 	
 	</div>
 </div>
- 
-
-
-
 </header>
 <!-- end custom menu --> 
 <div>
